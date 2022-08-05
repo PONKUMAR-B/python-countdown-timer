72e8290b1ce3cf2659da0246aef060338d041705
Ponkumar <ponkumarbalasubramanian@gmail.com>
2022-08-05 21:59:26 +0530

    coundown timer

--- countdown.py
+++ countdown.py
@@ -2,11 +2,11 @@ import time
 
 def countdown(t):
 
-    while t:
+    while(t>0):
 
         mins, secs = divmod(t, 60)
 
-        timer = '{:02d}:{:02d}'.format(mins,secs)
+        timer = '{}:{}'.format(mins,secs)
 
         print(timer, end="\r")
 
